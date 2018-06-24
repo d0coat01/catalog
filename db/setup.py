745cbbf90961f560afb31a1bd40dedbb3ce09139
vagrant <vagrant@vagrant.vm>
2018-06-24 22:24:56 +0000

    Added Category class, routes, and started template work.

--- db/setup.py
+++ db/setup.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -11,6 +11,7 @@ Base = declarative_base()
 
 secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
 
+
 # TODO: Create tests for user table
 class User(Base):
     __tablename__ = "user"
@@ -20,6 +21,7 @@ class User(Base):
     password = Column(String(64), nullable=False)
     first_name = Column(String(100))
     last_name = Column(String(100))
+    is_admin = Column(Boolean, default=False)
 
     def __init__(self, email, password):
         self.email = email
@@ -46,7 +48,26 @@ class User(Base):
             return None
         return data['id']
 
-# TODO: Create Catalog
+# TODO: Create Category
+
+
+class Category(Base):
+    __tablename__ = "category"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), unique=True, nullable=False)
+    label = Column(String(100), nullable=False)
+
+    def __init__(self, label):
+        self.label = label
+        self.name = label.lower()
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'label': self.label,
+            'id': self.id,
+        }
 # TODO: Create Item
 engine = create_engine('sqlite:///catalog.db')
 
