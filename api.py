39217b1d9d0fc1a292bb22684e3c53a703c2ae84
vagrant <vagrant@vagrant.vm>
2018-06-26 01:14:58 +0000

    Redesigned catalog page. Added routing to categories

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 import sys
 import bleach
 sys.path.append('./db')
-from setup import Base, User, Category
+from setup import Base, User, Category, Item
 from flask import session as login_session
 import random
 import string
@@ -34,6 +34,14 @@ def Catalog():
     username = login_session['username'] if 'username' in login_session.keys() else None
     return render_template('catalog.html', categories=categories, username=username)
 
+@app.route('/catalog/<string:category_name>/items')
+def CategoryItems(category_name):
+    session = DBSession()
+    category = session.query(Category).filter_by(name=bleach.clean(category_name)).one()
+    categories = session.query(Category).all()
+    items = session.query(Item).filter_by(category_id=category.id).all()
+    username = login_session['username'] if 'username' in login_session.keys() else None
+    return render_template('catalog.html', items=items, categories=categories, username=username, category=category)
 # Admin access only.
 @app.route('/categories')
 def Categories():
@@ -46,6 +54,7 @@ def CategoriesJSON():
     session = DBSession()
     categories = session.query(Category).all()
     return jsonify(Categories=[r.serialize for r in categories])
+
 @app.route('/categories/new', methods=['GET', 'POST'])
 def newCategory():
     session = DBSession()
@@ -73,6 +82,7 @@ def editCategory(category_name):
         session.commit()
         flash(category.label + " updated.")
         return redirect(url_for('Categories'))
+
 @app.route('/categories/<string:category_name>/delete', methods=['GET', 'POST'])
 def deleteCategory(category_name):
     session = DBSession()
@@ -186,76 +196,6 @@ def gconnect():
     print("done!")
     return output
 
-@app.route('/fbconnect', methods=['POST'])
-def fbconnect():
-    if request.args.get('state') != login_session['state']:
-        response = make_response(json.dumps('Invalid state parameter.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    access_token = request.data
-    print "access token received %s " % access_token
-    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
-        'web']['app_id']
-    app_secret = json.loads(
-        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
-        app_id, app_secret, access_token)
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
-
-
-    # Use token to get user info from API
-    userinfo_url = "https://graph.facebook.com/v2.8/me"
-    '''
-        Due to the formatting for the result from the server token exchange we have to
-        split the token first on commas and select the first index which gives us the key : value
-        for the server access token then we split it on colons to pull out the actual token value
-        and replace the remaining quotes with nothing so that it can be used directly in the graph
-        api calls
-    '''
-    token = result.split(',')[0].split(':')[1].replace('"', '')
-
-    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
-    # print "url sent for API access:%s"% url
-    # print "API JSON result: %s" % result
-    data = json.loads(result)
-    login_session['provider'] = 'facebook'
-    login_session['username'] = data["name"]
-    login_session['email'] = data["email"]
-    login_session['facebook_id'] = data["id"]
-
-    # The token must be stored in the login_session in order to properly logout
-    login_session['access_token'] = token
-
-    # Get user picture
-    url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
-    data = json.loads(result)
-
-    login_session['picture'] = data["data"]["url"]
-
-    # see if user exists
-    user_id = getUserId(login_session['email'])
-    if not user_id:
-        user_id = createUser(login_session)
-    login_session['user_id'] = user_id
-
-    output = ''
-    output += '<h1>Welcome, '
-    output += login_session['username']
-
-    output += '!</h1>'
-    output += '<img src="'
-    output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
-
-    flash("Now logged in as %s" % login_session['username'])
-    return output
-
-
 # Disconnect based on provider
 @app.route('/disconnect')
 def disconnect():
@@ -277,16 +217,6 @@ def disconnect():
     else:
         flash("You were not logged in")
         return redirect(url_for('Catalog'))
-
-@app.route('/fbdisconnect')
-def fbdisconnect():
-    facebook_id = login_session['facebook_id']
-    # The access token must me included to successfully logout
-    access_token = login_session['access_token']
-    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
-    h = httplib2.Http()
-    result = h.request(url, 'DELETE')[1]
-    return "you have been logged out"
 
 @app.route('/gdisconnect')
 def gdisconnect():
